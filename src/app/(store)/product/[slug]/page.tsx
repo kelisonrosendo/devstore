93c917b45e309ea49bc7e253443b10c053acb94e
Kelison Rosendo da Rocha <kelisonrosendo@gmail.com>
2025-10-14 10:41:10 -0300

    feat: adicionando ao carrinho

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import { api } from "@/data/api";
 import { Product } from "@/data/types";
-import Image from "next/image";
+import { AddToCartButton } from "@/components/add-to-cart-button";
 
 interface ProductProps {
   params: {
@@ -72,12 +73,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   );

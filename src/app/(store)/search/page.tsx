f34fe42c87699c4f2004a4ead62acd978e44bf50
Kelison Rosendo da Rocha <kelisonrosendo@gmail.com>
2025-10-14 11:48:43 -0300

    feat: fetch da página de busca

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,36 +1,66 @@
+import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { Product } from "@/data/types";
+import { api } from "@/data/api";
 
-export default function Search() {
+interface SearchProps {
+  searchParams: {
+    q: string;
+  };
+}
+
+async function searchhProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: { revalidate: 60 * 60 },
+  });
+
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = await searchParams;
+
+  if (!query) {
+    redirect("/");
+  }
+
+  const products = await searchhProducts(query);
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para: <span className="font-semibold">moletão</span>
+        Resultados para: <span className="font-semibold">{query}</span>
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/moletom`}
-          className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image
-            src=""
-            alt=""
-            className="group-hover:scale-105 duration-500"
-            width={920}
-            height={920}
-          />
+        {products.map((product) => (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              src={product.image}
+              alt={product.title}
+              className="group-hover:scale-105 duration-500"
+              width={400}
+              height={400}
+            />
 
-          <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">title</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(10).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </span>
-          </div>
-        </Link>
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
